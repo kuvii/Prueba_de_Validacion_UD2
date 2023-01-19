be9c80e0f1be61d68a337cd24d757528cb17c908
kuvii <kuvilet@gmail.com>
2023-01-19 10:47:40 +0000

    Refactor(CalificationCreator): Apply good practices

--- src/Clases/CalificationCreator.java
+++ src/Clases/CalificationCreator.java
@@ -1,28 +1,37 @@
 package Clases;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CalificationCreator extends Thread {
-    private String id;
-    private int STARTING_INDEX;
-    private int TOTAL_CREATION_NUM;
+    private String threadName;
+    private int startingIndex;
+    private int maxCreateNum;
 
-    public CalificationCreator(String id, int starting_index, int TOTAL_CREATION_NUM) {
-        this.id = id;
-        this.STARTING_INDEX = starting_index;
-        this.TOTAL_CREATION_NUM = TOTAL_CREATION_NUM;
+    public CalificationCreator(String id, int starting_index, int maxCreateNum) {
+        this.threadName = id;
+        this.startingIndex = starting_index;
+        this.maxCreateNum = maxCreateNum;
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    public int getStartingIndex() {
+        return startingIndex;
+    }
+
+    public int getMaxCreateNum() {
+        return maxCreateNum;
     }
 
     @Override
     synchronized public void run() {
-        int counter = this.STARTING_INDEX;
-        for (int i = this.STARTING_INDEX; i < TOTAL_CREATION_NUM; i++) {
+        int counter = getStartingIndex();
+        for (int i = getStartingIndex(); i < getMaxCreateNum(); i++) {
             int randomCalification = (int) (Math.random() * 10);
             CalificationHandler.addCalification(randomCalification);
             counter++;
         }
-        System.out.println("El hilo " + this.id + " que empezó en el indice " + this.STARTING_INDEX + " ha contado " + counter + " calificaciones");
+        System.out.println("El hilo " + getThreadName() + " que empezó en el indice " + getStartingIndex() + " ha contado " + counter + " calificaciones");
 
 
     }
